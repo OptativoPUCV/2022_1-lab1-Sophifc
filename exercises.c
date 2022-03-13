338e358a79091bd8b363397f19832cfd96945ad7
Sophifc <>
2022-03-13 12:49:18 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -108,7 +108,10 @@ Función que suma los vectores `a` y `b` y
 actualiza el vector `c` con el resultado de la suma.
 */
 void sumaV(Vector * a, Vector * b, Vector * c) {
-
+  int largo=a->capacidad;
+  for(int i=0, i<largo,i++){
+    c->datos[i]=a->datos[i]+b->datos[i];
+  }
 }
 
 /*
